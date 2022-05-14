f1d14b5d07c553b1bc0e87bbf079462dd8d79847
Colt <98503459+coltschultz@users.noreply.github.com>
2022-05-13 20:37:10 -0500

    add front-end javascript

---
+++ public/javascript/add-appointment.js
@@ -0,0 +1,25 @@
+async function newAppointmentHandler(event) {
+    event.preventDefault();
+  
+    const title = document.querySelector('input[name="appointment-title"]').value;
+    const post_url = document.querySelector('input[name="appointment-url"]').value;
+  
+    const response = await fetch(`/api/appointments`, {
+      method: 'POST',
+      body: JSON.stringify({
+        title,
+        post_url
+      }),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    });
+  
+    if (response.ok) {
+      document.location.replace('/dashboard');
+    } else {
+      alert(response.statusText);
+    }
+  }
+  
+  document.querySelector('.new-appointment-form').addEventListener('submit', newAppointmentHandler);
