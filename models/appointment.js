9f90fa7a7e2f14842118785098ce81fdd7a1e589
Colt <98503459+coltschultz@users.noreply.github.com>
2022-05-13 22:35:18 -0500

    link doctors to appointments

--- models/appointment.js
+++ models/appointment.js
@@ -1,36 +1,7 @@
 const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
 
-class Appointment extends Model {
-  static upvote(body, models) {
-    return models.Vote.create({
-      user_id: body.user_id,
-      post_id: body.post_id
-    }).then(() => {
-      return Appointment.findOne({
-        where: {
-          id: body.post_id
-        },
-        attributes: [
-          'id',
-          'post_url',
-          'title',
-          'created_at'
-        ],
-        include: [
-          {
-            model: models.Note,
-            attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
-            include: {
-              model: models.User,
-              attributes: ['username']
-            }
-          }
-        ]
-      });
-    });
-  }
-}
+class Appointment extends Model {}
 
 Appointment.init(
   {
@@ -56,6 +27,13 @@ Appointment.init(
       type: DataTypes.INTEGER,
       references: {
         model: 'user',
+        key: 'id'
+      }
+    },
+    doctor_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'doctor',
         key: 'id'
       }
     }
