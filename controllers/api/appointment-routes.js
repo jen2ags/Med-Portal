a4d943dbd73d96453499803cd289047ea2a57cfd
jennj <jennjenn93@gmail.com>
2022-05-14 00:22:58 -0500

    utils/auth.js

--- controllers/api/appointment-routes.js
+++ controllers/api/appointment-routes.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 const sequelize = require('../../config/connection');
-const withAuth = require('../../utils/auth');
+const withAuth = require('../../utils/auth.js');
 const { Appointment, User, Note } = require('../../models');
 
 // get all users
--- controllers/dashboard-routes.js
+++ controllers/dashboard-routes.js
@@ -1,7 +1,7 @@
 const router = require('express').Router();
 const sequelize = require('../config//connection');
 const { Post, User, Comment } = require('../models');
-const withAuth = require('../utils/auth');
+const withAuth = require('../utils/auth.js');
 
 router.get('/', withAuth, (req, res) => {
     Post.findAll({
