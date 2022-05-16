e3adaf954c965de97fdb6b692fd099e3ef616c14
Colt <98503459+coltschultz@users.noreply.github.com>
2022-05-16 12:33:34 -0500

    make users login immediately

--- controllers/index.js
+++ controllers/index.js
@@ -6,9 +6,12 @@ const homeRoutes = require('./home-routes.js');
 
 const appointmentRoutes = require('./appointment-routes.js');
 
+const viewAllRoutes = require('./view-all-routes.js');
+
 router.use('/', homeRoutes);
 router.use('/api', apiRoutes);
 router.use('/appointment', appointmentRoutes);
+router.use('/view-all', viewAllRoutes);
 
 router.use((req,res) => {
     res.status(404).end();
