ae0b6443fcefe21f1da6ce9c68daab0c9167c232
koiclone <cheryljcruz1@gmail.com>
2022-05-15 22:25:36 -0500

    changed seed data title

--- seeds/appointment-seeds.js
+++ seeds/appointment-seeds.js
@@ -2,42 +2,42 @@ const { Appointment } = require("../models");
 
 const appointmentdata = [
   {
-    title: "IBS issues",
+    title: "Patient name",
     date: "2022-06-22",
     time: "11:00AM",
     user_id: 1,
     doctor_id: 1,
   },
   {
-    title: "Sinus issues",
+    title: "Patient name",
     date: "2022-06-22",
     time: "12:00AM",
     user_id: 2,
     doctor_id: 2,
   },
   {
-    title: "Reproductive issues",
+    title: "Patient name",
     date: "2022-06-22",
     time: "10:00AM",
     user_id: 3,
     doctor_id: 3,
   },
   {
-    title: "Knee issues",
+    title: "Patient name",
     date: "2022-06-22",
     time: "01:00AM",
     user_id: 4,
     doctor_id: 4,
   },
   {
-    title: "Flea issues",
+    title: "Patient name",
     date: "2022-06-22",
     time: "02:00AM",
     user_id: 5,
     doctor_id: 5,
   },
   {
-    title: "Ear issues",
+    title: "Patient name",
     date: "2022-06-22",
     time: "03:00AM",
     user_id: 6,
