2708faafa0f2b984644e73937db92b8a1901fd39
koiclone <cheryljcruz1@gmail.com>
2022-05-16 20:09:07 -0500

    added patient, doctor name and date_time query selector

--- public/javascript/add-appointment.js
+++ public/javascript/add-appointment.js
@@ -2,13 +2,15 @@ async function newAppointmentHandler(event) {
     event.preventDefault();
   
     const title = document.querySelector('input[name="appointment-title"]').value;
-    const post_url = document.querySelector('input[name="appointment-url"]').value;
-  
+    const patient_name = document.querySelector('input[name="patient-name"]').value;
+    const doctor_name = document.querySelector('input[name="doctor-name"]').value;
+    const date_time = document.querySelector('input[name="date"]').value;
+  // add const for patient name doctor name date_time
     const response = await fetch(`/api/appointments`, {
       method: 'POST',
       body: JSON.stringify({
         title,
-        post_url
+        
       }),
       headers: {
         'Content-Type': 'application/json'
