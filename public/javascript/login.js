ea8fdb98938d2b78a679b9cc3b2e56d9c9b558d2
Colt <98503459+coltschultz@users.noreply.github.com>
2022-05-18 15:29:37 -0500

    formatting

--- public/javascript/login.js
+++ public/javascript/login.js
@@ -1,28 +1,25 @@
 async function loginUser(event) {
-    event.preventDefault();
-  
-    const email = document.querySelector('#email-login').value.trim();
-    const password = document.querySelector('#password-login').value.trim();
-  
-    if (email && password) {
-      const response = await fetch('/api/users/login', {
-        method: 'post',
-        body: JSON.stringify({
-          email,
-          password
-        }),
-        headers: { 'Content-Type': 'application/json' }
-      });
-  
-      if (response.ok) {
-        document.location.replace('/appointment')
-      } else {
-        alert(response.statusText);
-      }
+  event.preventDefault();
+  // Get form values
+  const email = document.querySelector("#email-login").value.trim();
+  const password = document.querySelector("#password-login").value.trim();
+  // Check if form complete
+  if (email && password) {
+    const response = await fetch("/api/users/login", {
+      method: "post",
+      body: JSON.stringify({
+        email,
+        password,
+      }),
+      headers: { "Content-Type": "application/json" },
+    });
+
+    if (response.ok) {
+      document.location.replace("/appointment");
+    } else {
+      alert(response.statusText);
     }
   }
-  
-  document.querySelector('#login-form').addEventListener('submit', loginUser);
-  
- 
-  
+}
+// Login User
+document.querySelector("#login-form").addEventListener("submit", loginUser);
--- public/javascript/new-patient.js
+++ public/javascript/new-patient.js
@@ -1,23 +1,26 @@
 async function signupUser(event) {
-    event.preventDefault();
-  
-    const patient_name = document.querySelector('#username-signup').value.trim();
-  
-    if (patient_name) {
-      const response = await fetch('/api/patients', {
-        method: 'post',
-        body: JSON.stringify({
-          patient_name
-        }),
-        headers: { 'Content-Type': 'application/json' }
-      });
-  
-      if (response.ok) {
-        document.location.replace('/appointment');
-      } else {
-        alert(response.statusText);
-      }
+  event.preventDefault();
+
+  // Get form values
+  const patient_name = document.querySelector("#username-signup").value.trim();
+
+  // Confirm form is complete
+  if (patient_name) {
+    const response = await fetch("/api/patients", {
+      method: "post",
+      body: JSON.stringify({
+        patient_name,
+      }),
+      headers: { "Content-Type": "application/json" },
+    });
+
+    if (response.ok) {
+      document.location.replace("/appointment");
+    } else {
+      alert(response.statusText);
     }
   }
-  
-  document.querySelector('#signup-button').addEventListener('click', signupUser);
+}
+
+// Sign Up Button Listener
+document.querySelector("#signup-button").addEventListener("click", signupUser);
